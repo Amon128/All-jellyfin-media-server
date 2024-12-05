db65e2da3192c413b07992edb2b6d5b461a52310
Amon128 <49078774+Amon128@users.noreply.github.com>
2024-12-04 20:46:34 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "making all directories"
+
+mkdir /dockerhdd/data
+mkdir /dockerhdd/data/configs
+
+mkdir /dockerhdd/data/gluetun
+
+mkdir /dockerhdd/data/qbittorrent
+mkdir /dockerhdd/data/qbittorrent/downloads
+
+mkdir /dockerhdd/data/sonarr
+mkdir /dockerhdd/data/sonarr/tv
+
+mkdir /dockerhdd/data/radarr
+mkdir /dockerhdd/data/sonarr/movies
+
+mkdir /dockerhdd/data/jellyfin
+mkdir /dockerhdd/data/jellyfin/cache
+
+mkdir /dockerhdd/data/configs/qbittorrent
+mkdir /dockerhdd/data/configs/prowlarr
+mkdir /dockerhdd/data/configs/jackett
+mkdir /dockerhdd/data/configs/sonarr
+mkdir /dockerhdd/data/configs/radarr
+mkdir /dockerhdd/data/configs/jellyfin
+mkdir /dockerhdd/data/configs/jellyseerr
+
+
+echo "end"
